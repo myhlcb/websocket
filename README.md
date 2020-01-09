d068969265016c91c9416c3b843ddfb21dbc06d9
myhlcb <1029785965@qq.com>
2020-01-09 16:38:14 +0800

    feat():start

---
+++ README.md
@@ -0,0 +1,4 @@
+## websocket
+* 部署 ws express 服务端
+* 部署 ws koa 服务端
+* 部署 express client端
---
+++ koa/app.js
@@ -0,0 +1,16 @@
+const Koa = require ('koa');
+const app = new Koa ();
+const http = require ('http').createServer (app.callback ());
+const io = require ('socket.io') (http);
+io.on ('connection', socket => {
+  console.log ('koa connect');
+});
+const Router = require ('koa-router');
+const router = new Router ();
+router.get ('/', ctx => {
+  ctx.body = 'hello';
+});
+app.use (router.routes ());
+http.listen (3001, () => {
+  console.log ('listen 3001');
+});
